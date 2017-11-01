e7675143f4dd7705cd85242b5b43f84b7ab8fb2a
Leon Waldman <le.waldman@gmail.com>
2017-11-01 12:36:35 -0300

    Version Bump and changelog update

--- lib/busbar_cli/config/version.rb
+++ lib/busbar_cli/config/version.rb
@@ -1,6 +1,6 @@
 # Version Variables
 VERSION                     = '1.5'.freeze
-BUSBAR_PATCH_VERSION        = '0'.freeze
+BUSBAR_PATCH_VERSION        = '1'.freeze
 KUBECTL_PATCH_VERSION       = '8'.freeze
 BUSBAR_VERSION              = "#{VERSION}.#{BUSBAR_PATCH_VERSION}".freeze
 KUBECTL_VERSION             = "#{VERSION}.#{KUBECTL_PATCH_VERSION}".freeze
