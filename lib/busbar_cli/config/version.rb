d03cf9dd70ec0393e3c426c35aa85d5a3dd54281
Leon Waldman <leon@waldman.io>
2017-12-14 15:00:38 -0300

    Upgrade kubectl version to 1.7 and Changelog Update

--- lib/busbar_cli/config/version.rb
+++ lib/busbar_cli/config/version.rb
@@ -1,6 +1,6 @@
 # Version Variables
-VERSION                     = '1.6'.freeze
+VERSION                     = '1.7'.freeze
 BUSBAR_PATCH_VERSION        = '0'.freeze
-KUBECTL_PATCH_VERSION       = '13'.freeze
+KUBECTL_PATCH_VERSION       = '11'.freeze
 BUSBAR_VERSION              = "#{VERSION}.#{BUSBAR_PATCH_VERSION}".freeze
 KUBECTL_VERSION             = "#{VERSION}.#{KUBECTL_PATCH_VERSION}".freeze
