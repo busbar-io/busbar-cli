9192a6d2e268910b3c41910068b0fba4875cd456
Leon Waldman <leon@waldman.io>
2018-08-27 18:57:17 -0300

    Improve description of "busbar resize" command

--- lib/busbar_cli/commands/resize.rb
+++ lib/busbar_cli/commands/resize.rb
@@ -4,7 +4,26 @@ module Commands
 
     included do
       desc 'resize APP ENV [COMPONENT_TYPE] NODE_TYPE',
-           'Change the current node type of an environment or a component'
+           'Change the current node type of an environment or a component (Available types: 1x, 2x, 4x, 1x.standard, 2x.standard, 4x.standard, 8x.standard, 1x.performance, 2x.performance, 4x.performance, 8x.performance)'
+      long_desc <<-LONGDESC
+           Change the current node type of an environment or a component.
+
+           Available types: 1x, 2x, 4x, 1x.standard, 2x.standard, 4x.standard, 8x.standard, 1x.performance, 2x.performance, 4x.performance, 8x.performance
+
+           Types description: \x5
+           1x > cpu: 1 / mem: 1500Mi \x5
+           2x > cpu: 2 / mem: 3000Mi \x5
+           4x > cpu: 4 / mem: 6000Mi \x5
+           1x.standard > cpu: 1 / mem: 500Mi \x5
+           2x.standard > cpu: 2 / mem: 1000Mi \x5
+           4x.standard > cpu: 4 / mem: 2000Mi \x5
+           8x.standard > cpu: 8 / mem: 4000Mi \x5
+           1x.performance > cpu: 1 / mem: 1500Mi \x5
+           2x.performance > cpu: 2 / mem: 3000Mi \x5
+           4x.performance > cpu: 4 / mem: 6000Mi \x5
+           8x.performance > cpu: 8 / mem: 12000M
+           LONGDESC
+
       method_option :profile,
                     desc: 'Profile used to run the command'
       def resize(
