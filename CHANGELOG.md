e1157173a42760098b101516e6366b877a3efe94
Leon Waldman <leon@waldman.io>
2019-07-30 21:05:19 -0300

    Fix 'busbar logs' issue with dashes on application name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## [1.9.4] - 2019-07-30
+### Fix
+- Fix busbar logs issue with dashes in the application name
+
 ## [1.9.3] - 2019-01-23
 ### Add
 - Add components to busbar show
--- lib/busbar_cli/services/logs.rb
+++ lib/busbar_cli/services/logs.rb
@@ -3,12 +3,34 @@ module Services
     def self.call(container_id:, environment_name:, since:)
       Services::Kube.setup
 
-      container_name = container_id.split('-')[0..-3].join('-')
-
-      Kernel.exec(
-        "#{KUBECTL} --context=#{Services::Kube.current_profile} " \
-        "logs -f --since=#{since} #{container_id} #{container_name} -n #{environment_name}"
+      # Fetch pod data and set container name
+      require 'open3'
+      stdout, stderr, status = Open3.capture3(
+        "#{KUBECTL} --context=#{Services::Kube.current_profile} --namespace #{environment_name} " \
+        "get pod #{container_id} --output json"
       )
+
+      # If ok => proceed
+      if status.to_s.end_with?('0') then
+        pod_data_json = JSON.parse(stdout)
+
+        if pod_data_json['spec']['containers'].length == 1 then
+          @container_name = pod_data_json['spec']['containers'][0]['name']
+        else
+          pod_data_json['spec']['containers'].each do |record|
+            @container_name = record['name'] unless record['name'].end_with?('nginx')
+          end
+        end
+
+        # Fetch container logs
+        Kernel.exec(
+          "#{KUBECTL} --context=#{Services::Kube.current_profile} --namespace #{environment_name} " \
+          "logs --follow=true --since=#{since} #{container_id} --container #{@container_name}"
+        )
+      else
+        puts "Error while retrieving Pod data:"
+        puts "  #{stderr}"
+      end
     end
   end
 end
